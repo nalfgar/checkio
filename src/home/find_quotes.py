d1f6b6a4b061a30cc22e2e293b43fc0eeb585ed6
Darek Strojecki <dstroj@wp.pl>
2020-04-15 15:34:08 +0200

    Add find_quotes function. Does not pass tests.

--- src/home/find_quotes.py
+++ src/home/find_quotes.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
 
+import re
 
 def find_quotes(text: str) -> list:
-    result = []
-    return result
+    print(text)
+    pattern = re.compile(r'""|"(\w+)"')
+
+    return re.findall(pattern, text)
+
+
 
 
--- src/home/find_quotes_test.py
+++ src/home/find_quotes_test.py
@@ -6,3 +6,36 @@ from src.home.find_quotes import find_quotes
 
 def test_word_without_quotes():
     assert find_quotes('Hi') == []
+
+
+def test_word_with_quotes():
+    assert find_quotes('"Hi"') == ['Hi']
+
+
+def test_three_word_with_quotes():
+    assert find_quotes('"Greetings"') == ['Greetings']
+    assert find_quotes('Hi') == []
+    assert find_quotes('good morning mister "superman"') == ['superman']
+    assert find_quotes('"this" doesn\'t make any "sense"') == ['this', 'sense']
+    assert find_quotes('"Lorem Ipsum" is simply dummy text '
+                       'of the printing and typesetting '
+                       'industry. Lorem Ipsum has been the '
+                       '"industry\'s standard dummy text '
+                       'ever since the 1500s", when an '
+                       'unknown printer took a galley of '
+                       'type and scrambled it to make a type '
+                       'specimen book. It has survived not '
+                       'only five centuries, but also the '
+                       'leap into electronic typesetting, '
+                       'remaining essentially unchanged. "It '
+                       'was popularised in the 1960s" with '
+                       'the release of Letraset sheets '
+                       'containing Lorem Ipsum passages, and '
+                       'more recently with desktop '
+                       'publishing software like Aldus '
+                       'PageMaker including versions of '
+                       'Lorem Ipsum.') == ['Lorem Ipsum',
+                                           "industry's standard dummy text ever "
+                                           'since the 1500s',
+                                           'It was popularised in the 1960s']
+    assert find_quotes('count empty quotes ""') == ['']
